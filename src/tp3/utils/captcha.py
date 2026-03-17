62a2067a81545da5b479019945a4cdb48fdf37fc
Théophane <theophane.legrand@siplec.leclerc>
2026-03-17 16:07:50 +0100

    complete captha function

--- src/tp3/utils/captcha.py
+++ src/tp3/utils/captcha.py
@@ -1,8 +1,15 @@
+import re
+from src.tp3.utils.logger import logger
+from PIL import Image
+from io import BytesIO
+import pytesseract
+
 class Captcha:
     def __init__(self, url):
         self.url = url
         self.image = ""
         self.value = ""
+        self.session = None
 
     def solve(self):
         """
@@ -14,6 +21,27 @@ class Captcha:
         """
         Fonction permettant la capture du captcha.
         """
+        response = self.session.get(self.url)
+        html = response.text
+        logger.debug(f"HTML: {html}")
+
+        match = re.search(r'<img[^>]+src="(.*?)"', html, re.IGNORECASE)
+        if match:
+            self.image_src = match.group(1)
+        else:
+            logger.error("Captcha image not found in HTML")
+            return
+
+        if self.image_src.startswith('http'):
+            image_url = self.image_src
+        else:
+            image_url = self.url.rsplit("/") + "/" + self.image_src.lstrip("/")
+        
+        logger.debug(f"Captcha image URL: {image_url}")
+
+        image_response = self.session.get(image_url)
+        self.image = Image.open(BytesIO(image_response.content))
+        logger.debug("Captcha image captured successfully")
 
     def get_value(self):
         """
