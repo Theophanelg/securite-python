fbbb3ef0a105f3ccadb852ee6a5a1b6cc9d63734
Théophane <theophane.legrand@siplec.leclerc>
2026-03-17 16:43:45 +0100

    complete function without valid flag

--- src/tp3/utils/session.py
+++ src/tp3/utils/session.py
@@ -1,4 +1,7 @@
 from src.tp3.utils.captcha import Captcha
+import re 
+import requests
+from src.tp3.utils.logger import logger
 
 
 class Session:
@@ -23,27 +26,51 @@ class Session:
         self.captcha_value = ""
         self.flag_value = ""
         self.valid_flag = ""
+        self.http_session = requests.Session()
+        self.response = None
 
     def prepare_request(self):
         """
         Prepares the request for sending by capturing and solving the captcha.
         """
         captcha = Captcha(self.url)
+        captcha.session = self.http_session
         captcha.capture()
         captcha.solve()
 
         self.captcha_value = captcha.get_value()
-        self.flag_value = "FIXME"
+        self.flag_value = ""
+        logger.debug(f"Captcha value: {self.captcha_value}")
 
     def submit_request(self):
         """
         Sends the flag and captcha.
         """
+        data = {
+            "captcha": self.captcha_value,
+            "flag": self.flag_value
+        }
+        self.response = self.http_session.post(self.url, data=data) 
+        logger.debug(f"Response status code: {self.response.status_code}")
 
     def process_response(self):
         """
         Processes the response.
         """
+        if self.response is None:
+            logger.error("No response to process")
+            return False
+
+        html = self.response.text
+        logger.debug(f"Response HTML: {html}")
+
+        match = re.search(r"[A-Z]+\{.*?\}", html, re.IGNORECASE)
+        if match:
+            self.valid_flag = match.group(0)
+            logger.debug(f"Valid flag found: {self.valid_flag}")
+            return True
+        logger.debug("No valid flag found")
+        return False
 
     def get_flag(self):
         """
